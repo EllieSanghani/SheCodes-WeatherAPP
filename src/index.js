9cfde9541298e89dcbd88c22e12ba6cc9dce586e
EllieSanghani <sangh@mac212589.local>
2020-09-22 22:32:50 +0100

    CurrentWeatherConditions
    JS Forcast started, updated current weather conditions API

--- src/index.js
+++ src/index.js
@@ -112,6 +112,29 @@ function showTemp(response) {
   document.querySelector(
     "#wind-speed"
   ).innerHTML = `Wind Speed: ${currentWindSpeed}mph`;
+
+  let conditions = response.data.weather[0].main;
+  document.querySelector(
+    "#currentWeather"
+  ).innerHTML = `Conditions: ${conditions}`;
+}
+
+function showForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+
+  forecastElement.innerHTML = ` 
+  
+        <div class="col-2">
+          <h1 class="emoji">🌦</h1>
+          <div class="col-12">
+            <h5 class="temp"><strong class="day">23° </strong> 12° </h5>
+            <div class="col-12">
+              <p>Monday</p>
+            </div>
+          </div>
+        </div>
+        <div class="vl"></div>
+  `;
 }
 
 function search(event) {
@@ -120,6 +143,9 @@ function search(event) {
   let cityName = document.querySelector("#search-city-input").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemp);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showForecast);
 }
 let currentTemp = document.querySelector("#search-form");
 currentTemp.addEventListener("submit", search);
